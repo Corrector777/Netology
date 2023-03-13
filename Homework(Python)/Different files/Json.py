a9890e9db38aab80b5f58235b971615a94ae69a1
Corrector777 <rlysogor84@gmail.com>
2023-03-14 01:24:38 +0300

    Теперь у нас в списке description слова длиной больше 6 символов

--- Homework(Python)/Different files/Json.py
+++ Homework(Python)/Different files/Json.py
@@ -1,10 +1,14 @@
 import json
 
 
-def read_json(file_path: str):
+def read_json(file_path, max_len_word=6):
     with open(file_path, encoding='utf 8') as f:
         news = json.load(f)
-        print(news)
-
+        words = []
+        for item in news['rss']['channel']['items']:
+            d = [word for word in item['description'].split(' ') if len(word) > max_len_word]
+            words.extend(d)
+        print(d)
+           
 
 read_json('1.json')
