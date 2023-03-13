adbf31eed2ec130a10c4bcc154f1fe324746109f
roman <rlysogor84@gmail.com>
2023-03-14 01:43:40 +0300

    kviz passed

--- Homework(Python)/Different files/Json.py
+++ Homework(Python)/Different files/Json.py
@@ -1,14 +1,16 @@
 import json
+import collections
 
 
-def read_json(file_path, max_len_word=6):
+def read_json(file_path, max_len_word=6, top_words=10):
     with open(file_path, encoding='utf 8') as f:
         news = json.load(f)
         words = []
         for item in news['rss']['channel']['items']:
             d = [word for word in item['description'].split(' ') if len(word) > max_len_word]
             words.extend(d)
-        print(d)
-           
+            counter_words = collections.Counter(words)
+        print(counter_words.most_common(top_words))
 
-read_json('1.json')
+           
+read_json('Netology/Homework(Python)/Different files/1.json')
