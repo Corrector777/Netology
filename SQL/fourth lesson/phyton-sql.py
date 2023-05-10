cde7ee45e7874553dc23256be9fe2396530315dd
roman <rlysogor84@gmail.com>
2023-05-10 22:26:59 +0300

    4th task started

--- SQL/fourth lesson/phyton-sql.py
+++ SQL/fourth lesson/phyton-sql.py
@@ -1,7 +1,9 @@
 import psycopg2
 
+with open('/Users/roman/Git/SQL_pass.ini') as file:
+    password = file.readline().strip()
 
-conn = psycopg2.connect(database="pythondb", user="postgres", password="fixator1") 
-with conn.cursor() as cur:
-    cur.execute("CREATE TABLE test(id SERIAL PRIMARY KEY);")
-    conn.commit()
+with psycopg2.connect(database="pythondb", user="postgres", password=password) as conn:
+    with conn.cursor() as cur:
+        cur.execute("CREATE TABLE test(id SERIAL PRIMARY KEY);")
+        conn.commit()
