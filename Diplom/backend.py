6597b6ba2420596007317565f738a90bdeb6e9df
Corrector777 <rlysogor84@gmail.com>
2023-06-17 18:39:56 +0300

    запрос недостающих данных

--- Diplom/backend.py
+++ Diplom/backend.py
@@ -1,23 +1,39 @@
 import vk_api
-
-
+from datetime import datetime
 from data import access_token
+from vk_api.exceptions import ApiError
+
 
 class VKApi:
 
     def __init__(self, access_token):
         self.vk_app = vk_api.VkApi(token=access_token)
-     
-
+ 
     def profile_info(self, user_id):
-        info = self.vk_app.method('users.get',
-                        {'user_id': user_id,
-                        'fields': 'city, bdate,sex,relation,home_town'})[0]
-        return info
+        try:
+            info = self.vk_app.method('users.get',
+                                    {'user_id': user_id,
+                                    'fields': 'city, sex, bdate'})[0]
+        
+        except ApiError as err:
+            info = {}
+            print(f'ошибка{err}')
+        birth_date = info.get('bdate') if info.get('bdate') is not None else None
+        user_age, = datetime.now().year - int(birth_date.split('.')[2]) if birth_date is not None and len(birth_date.split('.')) == 3 else None,
+        
+        result = {'id': info.get('id'),
+                  'name': f"{info.get('first_name')}  {info.get('last_name')}",
+                  'city': None,     #info.get('city')['title'] if info.get('city') is not None else None,
+                  'sex': info.get('sex'),
+                  'age': user_age
+                  }
+        # if result['age'] == None:
+
+        return result
 
 
 if __name__ == '__main__':
 
-    tools = VKApi(access_token)
     user_id = 1546753
-    print(tools.profile_info(user_id))
+    vk_app = VKApi(access_token)
+    print(vk_app.profile_info(user_id))
--- Diplom/interface.py
+++ Diplom/interface.py
@@ -1,9 +1,9 @@
 
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_api.utils import get_random_id
+from random import randrange 
 
-# from backend import VKApi
+from backend import VKApi
 from data import group_token, access_token
 
 greeting = ['привет', 'здравствуйте', 'хелоу', 'хай', 'hi', 'дд', 'hello']
@@ -14,15 +14,16 @@ class Interface():
     def __init__(self, group_token):
         self.vk_interface = vk_api.VkApi(token=group_token)
         self.longpoll = VkLongPoll(self.vk_interface)
+        self.vk_backend = VKApi(access_token)
+        self.my_user_info = {}
         
     def message_send(self, user_id, message, attachment=None):
         self.vk_interface.method('messages.send',
                        {'user_id': user_id,
                         'message': message,
                         'attachment': attachment,
-                        'random_id': get_random_id()})
+                        'random_id': randrange(10 ** 10)})
         
-
     def events_handling(self):
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -30,8 +31,31 @@ class Interface():
                 message = event.text.lower()
                 if message in greeting:
                     self.message_send(user, 'Приветствую тебя, искатель необъятной любви.')
+                    self.my_user_info = self.vk_backend.profile_info(user)
+                    
+                    self.message_send(user, 'Что нам о Вас известно:')
+                    self.message_send(user, f' Ваше имя: {self.my_user_info["name"]}')
+                    if self.my_user_info["age"] is not None:
+                        self.message_send(user, f' Ваш возраст: {self.my_user_info["age"]}')
+                    else:
+                        self.message_send(user, f' Возраста не хватает, введите:')
+                        self.my_user_info['age'] = event.text.title()
+                    self.message_send(user, f' Ваш пол: {self.my_user_info["sex"]}')
+                    if self.my_user_info["age"] is not None:
+                        self.message_send(user, f' Ваш город: {self.my_user_info["city"]}\n_____________________')
+                    else:
+                        for event in self.longpoll.listen():
+                            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                                self.message_send(user, f' Города нет, введите:')
+                                self.my_user_info['city'] = event.text.title()
+                            break
                 elif message == 'поиск':
-                    self.message_send(user, f'Приступаю к поиску пары для пользователя {user}')
+                    self.message_send(user, f'Давайте что-то вам подыщем\n_____________________')
+                    self.message_send(user, f' \nИскать будем:\n'
+                                            f' - пользователя противоположного пола\n'
+                                            f'- из города : {self.my_user_info["city"]}\n'
+                                            f'- возраст в диапазоне от {self.my_user_info["age"] -3} до {self.my_user_info["age"] + 3}')
+                                                          
                 else:
                     self.message_send(user, '''Комманда неизвестна.
                                                     ___________________________________________
